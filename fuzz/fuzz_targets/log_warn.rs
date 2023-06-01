b8430ab89838a5f6dd91ee6e3fdba67b7e9ad510
derekirish3 <lcfisch2@asu.edu>
2023-05-31 21:20:05 -0700

    Mayhem Support

---
+++ fuzz/fuzz_targets/log_warn.rs
@@ -0,0 +1,12 @@
+use honggfuzz::fuzz;
+use paris::Logger;
+
+fn main() {
+    let mut log = Logger::new();
+    loop {
+        fuzz!(|data: &[u8]| {
+            let input: &str = std::str::from_utf8(data).unwrap();
+            log.warn(input);
+        });
+    }
+}
